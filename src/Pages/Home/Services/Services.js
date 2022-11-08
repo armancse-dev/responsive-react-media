e41cd84abe948529305976bf38050711b0051f21
armancse-dev <armanhossaincse7@gmail.com>
2022-11-08 21:17:34 +0600

    Collect, Edit, optimize and host images for your website

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import firstService from '../../../images/services/1.jpg';
 
 const services = [
-   {name: 'Replace Tire', price: 2000, time: 2, img: ''}
+   {name: 'Replace Tire', price: 2000, time: 2, img: 'https://ibb.co/yF7wjBX'},
+   {name: 'Replace Tire', price: 2000, time: 2, img: 'https://ibb.co/QfdkWsR'},
+   {name: 'Replace Tire', price: 2000, time: 2, img: 'https://ibb.co/ZBGj48g'},
+   {name: 'Replace Tire', price: 2000, time: 2, img: 'https://ibb.co/JBnYyQV'},
+   {name: 'Replace Tire', price: 2000, time: 2, img: 'https://ibb.co/M1Q23mC'}
 ]
 
 const Services = () => {
