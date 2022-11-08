cfabc978a3a681ef15090e5dc6f2e708b7e915a8
armancse-dev <armanhossaincse7@gmail.com>
2022-11-08 20:57:01 +0600

    Best folder structure and component orientation

---
+++ src/Pages/Home/Services/Services.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const services = [
+   {name: 'Replace Tire', price: 2000, time: 2, img: ''}
+]
+
+const Services = () => {
+   return (
+      <div>
+         
+      </div>
+   );
+};
+
+export default Services;
